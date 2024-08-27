b111a2005d45ca69ea8640dfae8eccb3200bda8d
Mael Lemire <mael.lemire@icloud.com>
2024-08-27 17:09:12 +0200

    page de connexion v1

--- FrontEnd/assets/js/api.js
+++ FrontEnd/assets/js/api.js
@@ -1,12 +1,26 @@
 const getWorks = () => {
     return fetch('http://localhost:5678/api/works')
-    .then(response => response.json())    
+    .then(response => response.json())
 }
 
 const getCategories = () => {
     return fetch('http://localhost:5678/api/categories')
-    .then(response => response.json())  
-    .then(data => [{id: -1, name: 'Tous'}].concat(data))  
+    .then(response => response.json())
+    .then(data => [{id: -1, name: 'Tous'}].concat(data))
 }
 
-export {getCategories, getWorks}
+const getToken = (email, password) => {
+    return fetch('http://localhost:5678/api/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({email, password})
+    })
+    .then(response => response.json())
+    .then(data => {
+        localStorage.setItem('token', data.token)
+    })
+}
+
+export {getCategories, getWorks, getToken}
--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -8,15 +8,17 @@ form.addEventListener('submit', (e) => {
     const password = e.target.password.value
 
     localStorage.setItem('token', email)
+    console.log(localStorage.getItem('token'))
+      window.location.href = 'index.html';
 
 
     /**
-     * TODO: 
+     * TODO:
      * - Créer une fonction dans api.js qui fait l'appel à l'API pour se connecter (récupérer le jeton)
      * - Stocker le jeton en localStorage
      * - Rediriger l'utilisateur sur la page d'accueil
      * - Adapter le style de la page d'accueil quand on est connecté (cad que le jeton est présent en localStorage)
-     * 
+     *
      */
 
 
@@ -25,3 +27,12 @@ form.addEventListener('submit', (e) => {
 
     console.log(email, password)
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+  const token = localStorage.getItem('token');
+  const editLink = document.getElementById('edit-link');
+
+  if (token) {
+      editLink.style.display = 'block';
+  }
+});
